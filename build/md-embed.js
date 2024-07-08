36f8ab23dcd6e39d44de25ef4f00b039eee9604f
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2024-07-08 12:40:20 +0100

    feat: basic YT embed

--- build/md-embed.js
+++ build/md-embed.js
@@ -4,11 +4,15 @@ export const mdEmbed = function (md) {
     const srcIndex = token.attrIndex("src");
     const src = token.attrs[srcIndex][1];
 
-    if (src.includes("youtube.com" || src.includes("youtu.be"))) {
+    if (src.includes("youtube.com") || src.includes("youtu.be")) {
       const url = new URL(src);
       const videoId = url.searchParams.get("v");
+      const playlistId = url.searchParams.get("list");
+
       if (videoId) {
         return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
+      } else if (playlistId) {
+        return `<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${playlistId}" frameborder="0" allowfullscreen></iframe>`;
       }
     }
 
