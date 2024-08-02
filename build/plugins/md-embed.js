d7a0d47f0521aa342dec61871c9f610c08ddbea0
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2024-08-02 18:34:56 +0100

    chore: fail (more) gracefully when processing youtu.be and twitter embeds
    Still render a placeholder instead of a frame, but at least don't crash and provide a link

--- build/plugins/md-embed.js
+++ build/plugins/md-embed.js
@@ -1,3 +1,22 @@
+const getYTVideoUrlFromSrc = (src) => {
+  if (src.includes("youtube.com")) {
+    const url = new URL(src);
+    const videoId = url.searchParams.get("v");
+    const playlistId = url.searchParams.get("list");
+
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    } else if (playlistId) {
+      return `https://www.youtube.com/embed/videoseries?list=${playlistId}`;
+    }
+  }
+
+  if (src.includes("youtu.be")) {
+    const videoId = src.split("/").at(-1).trim();
+    return `https://www.youtube.com/embed/${videoId}`;
+  }
+};
+
 export const mdEmbed = function (md) {
   md.renderer.rules.image = function (tokens, idx, options, env, self) {
     const token = tokens[idx];
@@ -5,17 +24,12 @@ export const mdEmbed = function (md) {
     const src = token.attrs[srcIndex][1];
 
     if (src.includes("youtube.com") || src.includes("youtu.be")) {
-      const url = new URL(src);
-      const videoId = url.searchParams.get("v");
-      const playlistId = url.searchParams.get("list");
+      const videoUrl = getYTVideoUrlFromSrc(src);
+      if (!videoUrl)
+        return `<div class="debug-alert">Missing YT URL: ${src}</div>`;
 
-      if (videoId) {
-        return `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
-      } else if (playlistId) {
-        return `<iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${playlistId}" frameborder="0" allowfullscreen></iframe>`;
-      }
+      return `<iframe width="560" height="315" src="${videoUrl}" frameborder="0" allowfullscreen></iframe>`;
     }
-
     if (src.includes("twitter.com")) {
       return ` <a class="debug-alert" href="${src}">Embedded tweet (work in progress!)  </a>`;
     }
