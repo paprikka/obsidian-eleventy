fd52aca871ffb8218442374ca66abb2d7de68ec2
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2024-08-30 11:45:25 +0100

    chore: add site meta/OG

---
+++ src/_data/author.js
@@ -0,0 +1,10 @@
+import siteData from "./site.js";
+
+export default {
+  name: "Rafał Pastuszak",
+  avatar: `${siteData.rootUrl}/assets/me-photo.jpg`,
+  social: {
+    twitter: "@rafalpast",
+    mastodon: "@raf@mastodon.cloud",
+  },
+};
---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+const domain = "new.untested.sonnet.io";
+export default {
+  rootUrl: `https://${domain}`,
+  buildTime: new Date(),
+  title: "Untested · Rafał Pastuszak",
+  description: "Projects, experiments and toys by Rafał Pastuszak",
+  name: "Untested",
+};
